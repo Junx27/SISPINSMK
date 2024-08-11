7fcb5b9a375f733d4351fd734d49ac2c0952002a
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2024-08-11 10:26:28 +0700

    js

---
+++ public/build/assets/GenerateQrCode-CAnIJ5iS.js
@@ -0,0 +1,1 @@
+import{r as a,M as s,j as e}from"./app-CfL7hliQ.js";function d(){const{getId:t,setGetId:r}=a.useContext(s);return e.jsxs("div",{children:[e.jsx("input",{type:"number",name:"",id:"",value:t,onChange:n=>r(n.target.value)}),e.jsx("a",{href:`/generate/${t}`,children:"generate kode"})]})}export{d as default};
