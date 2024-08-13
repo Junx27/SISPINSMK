731762db29ea6f3ee9bb4ccbfc8a4c6496e4a5ae
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2024-08-13 13:22:53 +0700

    production

---
+++ public/build/assets/Url-Bx3CnFKe.js
@@ -0,0 +1,1 @@
+const t="https://junxwebdev.my.id/storage/";export{t as u};
--- resources/js/Data/Url.js
+++ resources/js/Data/Url.js
@@ -1,2 +1,2 @@
-export const url = "http://127.0.0.1:8000/storage/";
-// export const url = "https://junxwebdev.my.id/storage/";
+// export const url = "http://127.0.0.1:8000/storage/";
+export const url = "https://junxwebdev.my.id/storage/";
